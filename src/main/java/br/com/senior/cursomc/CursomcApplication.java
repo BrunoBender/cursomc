fd7ab320680f6f22f76506845215ebb0289b345b
Bruno Paulo Bender <brunopaulobender@gmail.com>
2021-05-31 12:02:05 -0300

    Criando profile de teste

--- src/main/java/br/com/senior/cursomc/CursomcApplication.java
+++ src/main/java/br/com/senior/cursomc/CursomcApplication.java
@@ -37,25 +37,6 @@ import br.com.senior.cursomc.services.ProdutoService;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
-	@Autowired
-	CategoriaService serviceCat;
-	@Autowired
-	ProdutoService serviceProd;
-	@Autowired
-	CidadeService serviceCid;
-	@Autowired
-	EstadoService serviceEst;
-	@Autowired
-	EnderecoService serviceEnd;
-	@Autowired
-	ClienteService serviceCli;
-	@Autowired
-	PedidoService servicePed;
-	@Autowired
-	PagamentoService servicepagto;
-	@Autowired
-	ItemPedidoService serviceItemP;
-	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -65,104 +46,7 @@ public class CursomcApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		
 		
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
-		Categoria cat3 = new Categoria(null, "Decoração");
-		Categoria cat4 = new Categoria(null, "Cama mesa e banho");
-		Categoria cat5 = new Categoria(null, "Eletrônicos");
-		Categoria cat6 = new Categoria(null, "Carnes");
-		Categoria cat7 = new Categoria(null, "Laticineos");
-		Categoria cat8 = new Categoria(null, "Queijos");
 		
-		Produto prod1 = new Produto(null, "Computador", 2000.00);
-		Produto prod2 = new Produto(null, "Impressora", 800.00);
-		Produto prod3 = new Produto(null, "Mouse", 80.00);
-		Produto prod4 = new Produto(null, "Mesa de escritório", 300.00);
-		Produto prod5 = new Produto(null, "Toalha", 50.00);
-		Produto prod6 = new Produto(null, "Colcha", 200.00);
-		Produto prod7 = new Produto(null, "TV true color", 1200.00);
-		Produto prod8 = new Produto(null, "Roçadeira", 800.00);
-		Produto prod9 = new Produto(null, "Abajour", 100.00);
-		Produto prod10 = new Produto(null, "Pendente", 180.00);
-		Produto prod11 = new Produto(null, "Shampoo", 90.00);
-		
-		
-		
-		cat1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
-		cat2.getProdutos().addAll(Arrays.asList(prod2,prod4));
-		cat3.getProdutos().addAll(Arrays.asList(prod5, prod6));
-		cat4.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3, prod7));
-		cat5.getProdutos().addAll(Arrays.asList(prod8));
-		cat6.getProdutos().addAll(Arrays.asList(prod9, prod10));
-		cat7.getProdutos().addAll(Arrays.asList(prod11));
-		
-		
-		prod1.getCategorias().addAll(Arrays.asList(cat1, cat4));
-		prod2.getCategorias().addAll(Arrays.asList(cat1, cat2, cat4));
-		prod3.getCategorias().addAll(Arrays.asList(cat1, cat4));
-		prod4.getCategorias().addAll(Arrays.asList(cat2));
-		prod5.getCategorias().addAll(Arrays.asList(cat3));
-		prod6.getCategorias().addAll(Arrays.asList(cat3));
-		prod7.getCategorias().addAll(Arrays.asList(cat4));
-		prod8.getCategorias().addAll(Arrays.asList(cat5));
-		prod9.getCategorias().addAll(Arrays.asList(cat6));
-		prod10.getCategorias().addAll(Arrays.asList(cat6));
-		prod11.getCategorias().addAll(Arrays.asList(cat7));
-		
-		serviceCat.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8));
-		serviceProd.saveAll(Arrays.asList(prod1, prod2, prod3, prod4, prod5, prod6, prod7, prod8, prod9, prod10 ,prod11));
-		
-		Estado est1 = new Estado(null, "Minas Gerais");
-		Estado est2 = new Estado(null, "São Paulo");
-		
-		Cidade cid1 = new Cidade(null, "Uberlândia", est1);
-		Cidade cid2 = new Cidade(null, "São Paulo", est2);
-		Cidade cid3 = new Cidade(null, "Campinas", est2);
-		
-		est1.getCidades().addAll(Arrays.asList(cid1));
-		est2.getCidades().addAll(Arrays.asList(cid2, cid3));
-		
-		
-		serviceEst.saveAll(Arrays.asList(est1, est2));
-		serviceCid.saveAll(Arrays.asList(cid1, cid2, cid3));
-		
-		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "2342423434", "maria@gmail.com", TipoCliente.PESSOA_FISICA);
-		cli1.getTelefones().addAll(Arrays.asList("234234234", "23432434"));
-		
-		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 300", "Jardim", "02394093", cli1, cid1);
-		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "23494093", cli1, cid2);
-
-		serviceCli.saveAll(Arrays.asList(cli1));
-		serviceEnd.saveAll(Arrays.asList(e1, e2));
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
-		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
-		
-		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
-		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
-		
-		ped1.setPagamento(pagto1);
-		ped2.setPagamento(pagto2);
-		
-		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
-		
-		servicePed.saveAll(Arrays.asList(ped1, ped2));
-		servicepagto.saveAll(Arrays.asList(pagto1, pagto2));
-		
-		ItemPedido ip1 = new ItemPedido(ped1, prod1, 0.00, 1, 2000.00);
-		ItemPedido ip2 = new ItemPedido(ped1, prod3, 0.00, 2, 80.00);
-		ItemPedido ip3 = new ItemPedido(ped2, prod2, 100.00, 1, 800.00);
-		
-		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
-		ped2.getItens().addAll(Arrays.asList(ip3));
-		
-		prod1.getItens().addAll(Arrays.asList(ip1));
-		prod2.getItens().addAll(Arrays.asList(ip3));
-		prod3.getItens().addAll(Arrays.asList(ip2));
-		
-		serviceItemP.saveAll(Arrays.asList(ip1, ip2, ip3));
 		
 		
 	}
